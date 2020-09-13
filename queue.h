2dfefcaf943d85f8f98821325755acb2378ad319
zhu849 <st81801@gmail.com>
2020-09-13 15:16:58 +0800

    Change q_size return value to q->size

--- queue.h
+++ queue.h
@@ -25,6 +25,7 @@ typedef struct ELE {
 /* Queue structure */
 typedef struct {
     list_ele_t *head; /* Linked list of elements */
+    int size;
     /* TODO: You will need to add more fields to this structure
      *        to efficiently implement q_size and q_insert_tail.
      */
