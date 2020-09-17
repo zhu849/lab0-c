68755b90e9bd7bb10fc438774c087fa6d2259d27
zhu849 <st81801@gmail.com>
2020-09-17 18:40:54 +0800

    Add q_remove_head() function

--- queue.c
+++ queue.c
@@ -100,7 +100,22 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
 {
     /* TODO: You need to fix up this code. */
     /* TODO: Remove the above comment when you are about to implement. */
+    if (!q || !(q->head)) {
+        return false;
+    }
+    list_ele_t *tmp = q->head;
     q->head = q->head->next;
+    tmp->next = NULL;
+    // sp is non-NULL, check real bufsize
+    if (sp) {
+        size_t realbufsize =
+            strlen(tmp->value) < bufsize ? strlen(tmp->value) : bufsize;
+        strncpy(sp, tmp->value, realbufsize);
+        sp[bufsize - 1] = '\0';
+    }
+    free(tmp->value);
+    free(tmp);
+    q->size--;
     return true;
 }
 
