affffdd2270861b582e70696460353de47b0ec41
zhu849 <st81801@gmail.com>
2020-09-17 18:48:41 +0800

    Modify q_size() function

--- queue.c
+++ queue.c
@@ -125,9 +125,8 @@ bool q_remove_head(queue_t *q, char *sp, size_t bufsize)
  */
 int q_size(queue_t *q)
 {
-    /* TODO: You need to write the code for this function */
-    /* Remember: It should operate in O(1) time */
-    /* TODO: Remove the above comment when you are about to implement. */
+    if (!q)
+        return 0;
     return q->size;
 }
 
